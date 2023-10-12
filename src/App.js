5d24677b75382c64819a1ddd75210cadb10b75e3
tarunmalik330 <maliktarun2005@gmail.com>
2023-10-12 11:09:48 +0530

    button-hover

--- src/App.js
+++ src/App.js
@@ -2,13 +2,11 @@
 import './App.css';
 import { Aboutus } from './components/Aboutus';
 import Faq from './components/Faq';
-import Headersection from './components/Headersection'
+import Headersection from './components/Headersection';
 import Imagesection from './components/Imagesection';
 import Roadmap from './components/Roadmap';
 import Supplies from './components/Supplies';
 import Team from './components/Team';
-
-
 
 function App() {
   return (
